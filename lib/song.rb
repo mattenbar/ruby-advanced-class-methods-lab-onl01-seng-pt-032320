a192997955ba61684fa3e309c48b907684ebe08d
matthew enbar <matthew.enbar@gmail.com>
2020-04-03 13:37:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,9 +35,9 @@ class Song
 
   def self.find_or_create_by_name(name)
     if @@all.include?(name)
-      find_by_name(name)
+      song.find_by_name(name)
     else
-      create_by_name(name)
+      song.create_by_name(name)
     end
   end
 
